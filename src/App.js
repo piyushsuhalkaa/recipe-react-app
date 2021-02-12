3da8232e9147b3419b1f1fc56126b147ae2fc7c5
Piyush Suhalka <piyushsuhalkaa@gmail.com>
2021-02-12 14:28:53 +0530

    Search Recipe Enabled

--- src/App.js
+++ src/App.js
@@ -6,43 +6,54 @@ const App = () => {
   const APP_ID = '7668791e'
   const APP_KEY = '8d793d68b2afe598b31db3e8c576deeb'
   const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('Indian Veg')
 
   useEffect(() => {
+    console.log('working')
     getRecipes()
-  }, [])
+  }, [query])
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=paneer&app_id=${APP_ID}&app_key=${APP_KEY}&to=5`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&to=5`
     )
     const data = await response.json()
     setRecipes(data.hits)
     console.log(data.hits)
   }
 
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
+
+  const getSearch = e => {
+    e.preventDefault()
+    setQuery(search)
+    setSearch('')
+  }
   return (
-    <>
-      <div className='App'>
-        <form className='search-form'>
-          <input
-            className='search-input'
-            type='text'
-            placeholder='Search Name'
-          />
-          <button className='search-btn' type='submit'>
-            Search
-          </button>
-        </form>
-        {recipes.map(recipe => (
-          <Recipe
-            key={recipe.recipe.label}
-            title={recipe.recipe.label}
-            calories={recipe.recipe.calories}
-            image={recipe.recipe.image}
-          />
-        ))}
-      </div>
-    </>
+    <div className='App'>
+      <form onSubmit={getSearch} className='search-form'>
+        <input
+          className='search-input'
+          type='text'
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className='search-btn' type='submit'>
+          Search
+        </button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
+    </div>
   )
 }
 
