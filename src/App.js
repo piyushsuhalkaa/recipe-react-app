18becb45b89e5427728d9a2512f0e678509a7662
Piyush Suhalka <piyushsuhalkaa@gmail.com>
2021-02-12 15:36:22 +0530

    New Css Changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&to=5`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&to=9`
     )
     const data = await response.json()
     setRecipes(data.hits)
@@ -36,23 +36,26 @@ const App = () => {
     <div className='App'>
       <form onSubmit={getSearch} className='search-form'>
         <input
-          className='search-input'
+          className='search-bar'
           type='text'
           value={search}
           onChange={updateSearch}
         />
         <button className='search-btn' type='submit'>
-          Search
+          <img src='./solid.svg'></img> Search
         </button>
       </form>
-      {recipes.map(recipe => (
-        <Recipe
-          key={recipe.recipe.label}
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image}
-        />
-      ))}
+      <div className='recipes'>
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            link={recipe.recipe.url}
+          />
+        ))}
+      </div>
     </div>
   )
 }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import style from './recipe.module.css'
 
-const Recipe = ({ title, calories, image }) => {
+const Recipe = ({ title, calories, image, link }) => {
   return (
-    <div>
+    <div className={style.recipe}>
       <h1>{title}</h1>
-      <p>{calories}</p>
       <img src={image} alt=''></img>
+      <p>{calories}</p>
+      <a href={link} target='_blank'>
+        Read More
+      </a>
     </div>
   )
 }
